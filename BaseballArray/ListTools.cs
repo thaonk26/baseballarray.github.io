b5a6d207381a4b6408acd9ced8d640778a26e9d9
Nathan <thaonk26@uww.edu>
2016-08-11 07:13:58 -0500

    initial uplaod

---
+++ BaseballArray/ListTools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballArray
+{
+    class ListTools
+    {
+        List<double> stats;
+        List<string> mlb;
+        public ListTools()
+        {
+            mlb = new List<string> { "Singles:", "Doubles:", "Triples:", "Home Runs:" };
+            stats = new List<double>();
+        }
+        public void addStats()
+        {
+            for (int i = 0; i < mlb.Count; i++)
+            {
+                Console.WriteLine("Please enter stats for {0}", mlb[i]);
+                stats.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+            printList();   
+        }
+        public void printList()
+        {
+            for(int i = 0; i < mlb.Count; i++)
+            {
+                Console.WriteLine(mlb[i] + " " + stats[i]);
+            }
+        }
+    }
+}
---
+++ BaseballArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListTools lists = new ListTools();
+            List<string> baseballStats = new List<string>();
+            lists.addStats();
+        }
+    }
+}
